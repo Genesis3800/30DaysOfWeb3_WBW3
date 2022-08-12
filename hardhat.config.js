3476fdf84323ac9096fcfd852acc33e5dbdd11ee
Priyank Gupta <Genesis3800>
2022-08-12 20:15:27 +0530

    WBW3 section 4 checkpoint

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+//My contract address as of now
+//https://mumbai.polygonscan.com/address/0xFbf777B8cc6C5cac050EC5e8ab9e2a2c06A0F32F#code
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [process.env.STAGING_PRIVATE_KEY],
+    },
+  },
 };
